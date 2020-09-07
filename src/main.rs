6a404f17c77e8378f286e88090b59d9be55175c8
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-09-06 18:25:23 -0600

    Cleanup works! (v1) - Need to check for dependancies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::fs;
+use std::path::Path;
 use std::process::Command;
 use std::str::FromStr;
 use structopt::StructOpt;
@@ -7,12 +7,13 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     cmd: Cmd,
+    path: String,
 }
 
 #[derive(Debug)]
 enum Cmd {
-    Cook,
     Cleanup,
+    Create,
 }
 
 #[derive(Debug)]
@@ -23,14 +24,13 @@ impl fmt::Display for ParseCmdError {
         write!(f, "Parse Command Error")
     }
 }
-
 impl FromStr for Cmd {
     type Err = ParseCmdError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "cook" => Ok(Cmd::Cook),
             "cleanup" => Ok(Cmd::Cleanup),
+            "create" => Ok(Cmd::Create),
             _ => Err(ParseCmdError),
         }
     }
@@ -39,45 +39,34 @@ impl FromStr for Cmd {
 fn main() {
     let args = Cli::from_args();
 
-    let cargo_contents = fs::read_to_string("Cargo.toml")
-        .expect("Could not read Cargo.toml! Is this a rust folder?");
-
-    // the name of the folder
-    let mut name: String = String::new();
-
-    for line in cargo_contents.lines() {
-        if line.contains("name =") {
-            name = line
-                .trim()
-                .split("=")
-                .map(|x| x.to_string())
-                .collect::<Vec<String>>()
-                .get(1)
-                .expect("Couldn't find the name of the folder you're in!")
-                .clone();
-        }
-    }
-
-    // DEBUG - making sure I read the commands right
-    println!("Kitchen is called {}", name);
-    println!("Cmd is {:?}", args.cmd);
-
     match args.cmd {
-        Cmd::Cook => {
-            // run cargo build
-            let mut cargo_cmd = Command::new("cargo");
-            let built = cargo_cmd
-                .arg("build")
-                .output()
-                .expect("failed to build project!");
-            println!("build status: {}", built.status);
+        Cmd::Cleanup => {
+            println!(
+                "Path to Cargo.toml is suggested to be: {}/Cargo.toml",
+                args.path
+            );
 
-            // run binary
-            println!("about to execute binary {}", name);
-            let mut execute_binary = Command::new(format!("./target/debug/{}", name));
-            let output = execute_binary.output().expect("Failed to run binary");
-            println!("status: {}", output.status);
+            // Check if it's a Rust folder (containing Cargo.toml)
+            if Path::new(&format!("{}/Cargo.toml", args.path)).exists() {
+                // copy the `main.rs` file to a `{folder}.rs` file
+                let copy = Command::new("cp")
+                    .arg(format!("{}/src/main.rs", args.path))
+                    .arg(format!("{}.rs", args.path))
+                    .output()
+                    .expect("Couldn't main.rs file!");
+
+                // delete everything in the folder
+                let rm = Command::new("rm")
+                    .arg("-rf")
+                    .arg(args.path)
+                    .output()
+                    .expect("Couldn't delete folder!");
+
+                println!("pog?")
+            } else {
+                println!("Kitchen couldn't find a Cargo.toml file, and thus couldn't determine whether or not it's a Rust folder. Evacuating...")
+            }
         }
-        Cmd::Cleanup => {}
+        Cmd::Create => println!("Create!"),
     }
 }
